79b977dbf80af6b64b4d15a84e991ef48b671745
Jason DeWitt <jason.dewitt@10up.com>
2017-09-03 18:52:04 -0500

    added Docker file for main app and configured the db layer

--- project/config.py
+++ project/config.py
@@ -1,22 +1,23 @@
-
+import os
 
 class BaseConfig:
     """Base Configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """Dev Config"""
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing Config"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Prod config"""
     DEBUG = False
-
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
